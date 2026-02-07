608738becc492fa88bf917941da846168da3d8fa
Dom Brassey (she/they) <dombrassey@gmail.com>
2026-02-07 09:28:28 -0800

    Add files via upload

---
+++ rosters.js
@@ -0,0 +1,13 @@
+// Rosters data for Random Grouper
+
+let rosters = [
+  {
+    label: "Game Design MW",
+    names: ["Umut Eslem", "Eun-woo Ji-hyun", "Bilge Min-seo", "Kyung Ji-an", "Jung Ebrar", "Dara Tola", "Chan Vanna", "Phalla Pich", "Sophon Chamroeun", "Samnang Sovann", "Sevan A", "Sevan V", "Vosgi Voski", "Arshaluys V", "Arshaluys S", "Toyin Adebola", "Ade Adetokunbo", "Enitan Toyin", "Adeyemi Adebola", "Adebisi Ekundayo"],
+  },
+
+  {
+    label: "Game Design TT",
+    names: ["Ji-hu", "Udo", "Tonalli", "Berhane", "Onalerona", "Mumtaz", "Kutemwa", "Oghenekaro", "Harisha", "Ceri", "Giusi", "Zhenya", "Masozi", "Katlego", "Fiore", "Jannūr", "Sophal", "Karuna", "Kalei", "Altangerel", "Krishna", "Aljoša", "Nthanda", "Eluney", "Clancy", "Lei", "Meilin", "Xin", "Haoxuan", "Haoxuan"],
+  },
+];
